33d7d05e8c27bc675881ed2da127f935e42d5a91
Your NameLinzhouLi <LinzhouLi@outlook.com>
2022-03-31 22:13:50 +0800

    添加烘焙贴图

--- js/Blur.js
+++ js/Blur.js
@@ -10,9 +10,11 @@ class Blur {
         bg.height = window.innerHeight;
 
         let width = 120, height = 50;
-        let button = document.createElement('p');//按钮
-        button.innerHTML = "Enter";
-        button.style.cssText =
+        let button1 = document.createElement('p');//按钮
+        let button2 = document.createElement('p');//按钮
+        button1.innerHTML = "Enter";
+        button2.innerHTML = "Coffee Break";
+        button1.style.cssText =
             `font-size: 20px;
             width: 120px;
             height: 50px;
@@ -25,19 +27,44 @@ class Blur {
             position: fixed;
             left: ${ window.innerWidth / 2 - width / 2 }px;
             top: ${ window.innerHeight / 2 - height / 2 }px;`;
+        button2.style.cssText =
+            `font-size: 15px;
+            width: 120px;
+            height: 50px;
+            color: white;
+            background: #2ECC71;
+            vertical-align: middle;
+            text-align: center;
+            line-height: 50px;
+            border-radius: 6px;
+            position: fixed;
+            left: ${ window.innerWidth - 10 - width }px;
+            top: ${ 0 }px;`;
 
-        button.onmouseover = function() {
-            button.style.cursor = 'pointer';
-            button.style.backgroundColor = '#81F79F';
+        document.getElementById("cheat2").style.display = 'none';
+
+        button1.onmouseover = function() {
+            button1.style.cursor = 'pointer';
+            button1.style.backgroundColor = '#81F79F';
+        }
+        button2.onmouseover = function() {
+            button2.style.cursor = 'pointer';
+            button2.style.backgroundColor = '#81F79F';
         }
 
-        button.onmousedown = function() {
+        button1.onmousedown = function() {
             document.getElementById("cheat").style.display = 'none';
+            document.getElementById("cheat2").style.display = 'inline';
             scope.started = true;
             if( scope.startFunc ) scope.startFunc();
         }
+        button2.onmousedown = function() {
+            document.getElementById("cheat2").style.display = 'none';
+            window.location.href = "showRoom/index.html";
+        }
 
-        document.getElementById("cheat").appendChild( button );
+        document.getElementById("cheat").appendChild( button1 );
+        document.getElementById("cheat2").appendChild( button2 );
 
     }
 }
